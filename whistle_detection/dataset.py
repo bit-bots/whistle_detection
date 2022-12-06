f6bc1fd3c2f072f8adf431d6ff358e3fb881b515
Hauke Schnau <hauke@schnau-lilienthal.de>
2022-12-06 17:46:43 +0100

    clean up and finish dataset

--- whistle_detection/dataset.py
+++ whistle_detection/dataset.py
@@ -1,68 +1,29 @@
 import json
 import os
-from pathlib import Path
 from torch.utils.data import Dataset
 import torchaudio
-
-# folder = 'data'
-# processed_folder = 'processed'
-
-# required_folders = [f'{processed_folder}/whistle', f'{processed_folder}/no_whistle']
-# for required_folder in required_folders:
-#     if not os.path.exists(required_folder):
-#         os.makedirs(required_folder, exist_ok=True)
-
-# SAMPLE_SIZE = 4096
-
-# audio_files = [Path(f'{folder}/{file_data["path"]}') for file_data in database['audioFiles']]
-
-# def get_whistle_labels(audio_file):
-#     file_data = next(file for file in database['audioFiles'] if file['path'] == str(audio_file))
-#     return file_data['channels'][0]['whistleLabels']
-
-# def contains_whistle(filename, start):
-#     end = start + SAMPLE_SIZE
-#     whistles = get_whistle_labels(filename)
-#     for whistle in whistles:
-#         if whistle['start'] <= start and whistle['end'] >= end:
-#             return True
-#     return False
-
-# def store_frames(wav, filename, start, label):
-#     wav.setpos(start)
-#     frames = wav.readframes(SAMPLE_SIZE)
-#     sample_rate = wav.getframerate()
-#     sample_width = wav.getsampwidth()
-#     channels = wav.getnchannels()
-#     with wave.open(f'{processed_folder}/{label}/{filename}_{start}.wav', 'wb') as out_wav:
-#         out_wav.setnchannels(channels)
-#         out_wav.setsampwidth(sample_width)
-#         out_wav.setframerate(sample_rate)
-#         out_wav.writeframes(frames)
-
-# for audio_file in audio_files:
-#     with wave.open(str(audio_file), 'rb') as wav:
-
-
-#         nframes = wav.getnframes()
-#         for i in range(0, nframes, SAMPLE_SIZE):
-#             if i + SAMPLE_SIZE > nframes:
-#                 break
-
-#             if contains_whistle(audio_file.name, i):
-#                 store_frames(wav, audio_file.stem, i, 'whistle')
-#             else:
-#                 store_frames(wav, audio_file.stem, i, 'no_whistle')
+import random
+import math
 
 class AudioDataset(Dataset):
-    def __init__(self, database_path, target_sample_rate=10_000, chunk_duration=1):
-        database = json.load(open(database_path, 'r'))
+    def __init__(self, database_path, target_sample_rate=10_000, chunk_duration=1, train_mode=True, train_test_split=0.8):
+        self.database = self.load_database(database_path, train_mode, train_test_split)
         self.folder = os.path.dirname(database_path)
-        self.files = [os.path.join(self.folder, file_data['path']) for file_data in database['audioFiles']]
         self.target_sample_rate = target_sample_rate
         self.chunk_duration = chunk_duration
         # dict of file name to tuple of waveform and sample rate
-        self.audio = {audio_file: self.resample(*torchaudio.load(audio_file)) for audio_file in self.files}
+        self.audio = {filename: self.resample(*torchaudio.load(filename)) for filename in self.database.keys()}
+
+    def load_database(self, database_path, train_mode, train_test_split):
+        database = json.load(open(database_path, 'r'))
+        split_index = math.floor(len(database['audioFiles']) * train_test_split)
+
+        if train_mode:
+            files = database['audioFiles'][:split_index]
+        else:
+            files = database['audioFiles'][split_index:]
+
+        return {audio_file["path"]: audio_file["channels"] for audio_file in files}
 
     def resample(self, waveform, sample_rate):
         channel = 0
@@ -70,17 +31,30 @@ class AudioDataset(Dataset):
 
     def __len__(self):
         total_samples = 0
-        for audio_file in self.audio.items():
-            total_samples += audio_file.shape[1]
+        for waveform, _ in self.audio.values():
+            total_samples += waveform.shape[1]
         duration = total_samples / self.target_sample_rate
         return duration // self.chunk_duration
+    
+    def get_whistle_labels(self, filename):
+        file_data = self.database[filename]
+        return file_data['channels'][0]['whistleLabels']
+    
+    def get_label(self, filename, start):
+        end = start + self.chunk_duration
+        for label in self.get_whistle_labels(filename):
+            if not label['start'] < start and label['end'] < start \
+                or label['start'] > end and label['end'] > end:
+                return True
+        return False
 
-    # TODO: Implement later
-    def __getitem__(self, idx):
-        file = self.files[idx]
-        with wave.open(f'{self.folder}/{file}', 'rb') as wav:
-            frames = wav.readframes(SAMPLE_SIZE)
-            sample_rate = wav.getframerate()
-            sample_width = wav.getsampwidth()
-            channels = wav.getnchannels()
-            return frames, sample_rate, sample_width, channels
+    def __getitem__(self, _):
+        filename = random.choice(self.audio.keys())
+        waveform = self.audio[filename]
+        start_pos = random.randint(0, waveform.shape[1] - self.target_sample_rate * self.chunk_duration)
+        chunk = waveform[:, start_pos:start_pos + self.target_sample_rate * self.chunk_duration]
+        label = self.get_label(filename, start_pos)
+
+        mel_spectrogram = torchaudio.transforms.MelSpectrogram(self.target_sample_rate)(chunk)
+
+        return mel_spectrogram, label
